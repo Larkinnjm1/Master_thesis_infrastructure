d05f469293a47ef52f792f9161536cd3c1d3c451
PG-Larkin <Niall.Larkin@city.ac.uk>
2019-11-18 23:04:18 +0000

    segcaps specific infrastructure added

--- SegCaps specific/rename_csv_files_diff_pats.py
+++ SegCaps specific/rename_csv_files_diff_pats.py
@@ -6,15 +6,16 @@ import subprocess
 import sys
 
 for root,subdir,files in os.walk():
-	file_chk=glob.glob(os.path.join(root,"*.csv"))
-	
-	if len(file_chk)>0:
-    	for file in file_chk:
+    
+    file_chk=glob.glob(os.path.join(root,"*.csv"))
+    
+    if len(file_chk)>0:
+        for file in file_chk:
             
             file_sub_str=['summary.csv','dice.csv']
             file_sub_str_lst=[x for x in file_sub_str if file.find(x)!=-1]
-
             if len(file_sub_str_lst)>0:
+                
                 file_renm='nonval_pat_32_3_'+os.path.basename(file)
                 file_renm=os.path.join(os.path.dirname(file),file_renm)
                 os.rename(file,file_renm)
