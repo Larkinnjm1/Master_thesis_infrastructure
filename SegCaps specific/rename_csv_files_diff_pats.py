d41edb0be3c3dcfbbb722b6f3f3c0d2ec8252cf3
PG-Larkin <Niall.Larkin@city.ac.uk>
2019-11-18 23:12:04 +0000

    additional items added wrt to segcaps boilerplate renaming files fro mlocal to s3 remote

--- SegCaps specific/rename_csv_files_diff_pats.py
+++ SegCaps specific/rename_csv_files_diff_pats.py
@@ -4,6 +4,7 @@ import os
 import glob
 import subprocess
 import sys
+import pickle
 
 for root,subdir,files in os.walk():
     
@@ -19,3 +20,27 @@ for root,subdir,files in os.walk():
                 file_renm='nonval_pat_32_3_'+os.path.basename(file)
                 file_renm=os.path.join(os.path.dirname(file),file_renm)
                 os.rename(file,file_renm)
+
+#Appending final values after renaming of file                
+file_var_loc=[]
+file_var_loc=subprocess.check_output('find . -iname nonval_pat_32_3_*',shell=True).decode(sys.stderr.encoding).split('\n')
+tmp_lst=[]
+for val in file_var_loc:
+    tmp_info=val.split('/')
+    tmp_lst.append(tmp_info[1])
+    
+tmp_lst_uniq=set(tmp_lst)
+
+#Finidng matching patterns in original pickle file for exporting
+with open('file_path_dict','rb') as fb:
+    local_s3_remote_dict=pickle.load(fb)
+    
+for k in list(local_s3_remote_dict.keys()):
+    if k not in tmp_lst_uniq:
+        del local_s3_remote_dict[k]
+
+#Syncing final values from local to s3 remote         
+for k,v in local_s3_remote_dict.items():
+    cmd='aws s3 sync {0} {1} --exclude "*" --include "*.png" --include "*.mha" --include ".csv"'.format(k,v)
+    subprocess.run(cmd,shell=True)
+
